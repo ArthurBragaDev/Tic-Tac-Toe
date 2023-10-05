6e6fdc4da35ba63c0cd38350117999ee0fccaaf6
Arthur Braga <ahbragadev@gmail.com>
2023-10-05 18:17:14 -0300

    Criando nova GameActivity

--- app/src/main/java/br/com/course/tictactoe/MainActivity.kt
+++ app/src/main/java/br/com/course/tictactoe/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.course.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.course.tictactoe.databinding.ActivityMainBinding
@@ -12,5 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.playOfflineBtn.setOnClickListener{
+            createOfflineGame()
+        }
     }
+
+    fun createOfflineGame(){
+        startGame()
+    }
+
+    fun startGame(){
+       startActivity(Intent(this, GameActivity::class.java))
+    }
+
+
 }
