4be58f894d2d24e01852edade8f8ff5ab6b8b4fe
Arthur Braga <ahbragadev@gmail.com>
2023-10-06 10:13:17 -0300

    configurando gamemodel, gameData

--- app/src/main/java/br/com/course/tictactoe/GameActivity.kt
+++ app/src/main/java/br/com/course/tictactoe/GameActivity.kt
@@ -2,10 +2,41 @@ package br.com.course.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import br.com.course.tictactoe.databinding.ActivityGameBinding
+import br.com.course.tictactoe.databinding.ActivityMainBinding
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var binding: ActivityGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btn0.setOnClickListener(this)
+        binding.btn1.setOnClickListener(this)
+        binding.btn2.setOnClickListener(this)
+        binding.btn3.setOnClickListener(this)
+        binding.btn4.setOnClickListener(this)
+        binding.btn5.setOnClickListener(this)
+        binding.btn6.setOnClickListener(this)
+        binding.btn7.setOnClickListener(this)
+        binding.btn8.setOnClickListener(this)
+
+
+        binding.startGameBtn.setOnClickListener{
+            startGame()
+        }
+
+    }
+
+    private fun startGame() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
